792a649c8c7ab6f90e48188d9e22b1ff8e7d6c4b
Hermitao <vkriegergranemann@gmail.com>
2022-02-23 15:42:38 -0300

    First update with actual code!

---
+++ DeepCoreIndustries/Source/DeepCoreIndustries/DeepCoreIndustries.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace DCI
+{
+    [StaticConstructorOnStartup]
+    public static class DeepCoreIndustries
+    {
+        static DeepCoreIndustries()
+        {
+            Log.Message("Hello, fucker!");
+        }
+    }
+}
